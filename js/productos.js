9d2e51c1a857710039353d964935b64b6b4ceac4
Bruno M <bmalagoli2@gmail.com>
2021-08-31 15:09:50 -0300

    Segundo commit

--- js/productos.js
+++ js/productos.js
@@ -1,30 +1,3 @@
-// // Obtiene el producto deseado y en cuantas cuotas se quiere pagar
-// let productoSeleccionado=prompt("Seleccione el producto que desea (Disponibles: RTX 3090, RTX 2090, Intel I7)")
-//  let cuotas=parseInt(prompt("Ingrese la cantidad de cuotas en las que desea pagar"))
-//   function obtenerPrecio(producto){
-//       let precio;
-//     switch(producto){
-//      case "RTX 3090":precio=300000;
-//          break;
-//       case "Intel I7":precio=75000;
-//          break;
-//      case "RTX 2090": precio=200000;
-//         break;
-//       default: alert("Este producto no esta disponible");
-//          break;
-//       }
-//       return precio;
-//   }
-//  //Devuelve el precio de cada cuota
-//   function obtenerCuotas(precio,cuotas){
-//      let precioCuotas=precio/cuotas;
-//         return precioCuotas;
-//  }
-//  let precioProducto=obtenerPrecio(productoSeleccionado);
-//  let precioPorCuota=obtenerCuotas(precioProducto, cuotas);
-//  alert("El precio del producto seria:"+" "+precioProducto+" "+"A pagar en" +" " +cuotas+" "+"cuotas de"+" "+precioPorCuota)
-
-
 
 //Array de productos
 const productos=[
@@ -34,13 +7,37 @@ const productos=[
     {id:4, producto:"Placa de video",marca:"AMD",modelo:"RX 580X",precio:120000,imagen:"../images/RadeonRX580.jpg"},
     {id:5, producto:"Memoria RAM",marca:"HyperX",modelo:"Fury",precio:6000,imagen:"../images/hyperxfury.jpg"}
 ]
-
+//Procesando Productos
 let seccion=document.getElementById("container__productos");
 for (const producto of productos){
      let seccionProductos=document.createElement("div");
      seccionProductos.setAttribute("class","productos")
      seccionProductos.innerHTML=`<img src="${producto.imagen}" class="productos__imagenes" alt="productos computacion">
                                  <h3 class="titulo__producto">${producto.marca} ${producto.modelo}</h3>
-                                 <p class="producto--texto">Precio $:${producto.precio}</p>`;
+                                 <p class="producto--texto">Precio $:${producto.precio}</p>
+                                 <button class="carrito__boton" type="button">Agregar al carrito</button>`;
      seccion.appendChild(seccionProductos);
  }
+
+ //
+ function filtroDesplegable(nombre,visibilidad){
+    let desplegable=document.getElementById(nombre);
+    if (visibilidad===false){
+        if(desplegable.classList.contains("oculto")){//Si desplegable contiene una clase con nombre oculto la borra y agrega una con nombre visible
+            desplegable.classList.remove("oculto")
+        }
+        desplegable.className +="visible"//Agrega la clase al final de la lista de clases del html
+    }else{
+        if(desplegable.classList.contains("visible")){//Si desplegable contiene una clase con nombre visible la borra y agrega una con nombre oculto
+            desplegable.classList.remove("visible")
+        }
+        desplegable.classList +="oculto"//Agrega la clase al final de la lista de clases del html
+    }
+}
+//Evento click para hacer aparecer el desplegable
+let botonDesplegable=document.getElementById("agregarFiltros");
+let desplegableVisible=false;
+botonDesplegable.onclick=()=>{
+    filtroDesplegable("aside__seleccion",desplegableVisible)//Agrega valores a los parametros
+    desplegableVisible=!desplegableVisible;//Si era false ahora es true y si era true ahora es false
+}
